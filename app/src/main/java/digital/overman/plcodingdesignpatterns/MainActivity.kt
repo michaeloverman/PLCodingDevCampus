5ab1f606e886267ae4d5465dd5c850cff6284a0d
Michael Overman <overman.michael@gmail.com>
2025-05-12 21:48:51 -0400

    mars weather in main activity for screen shot

--- app/src/main/java/digital/overman/plcodingdesignpatterns/MainActivity.kt
+++ app/src/main/java/digital/overman/plcodingdesignpatterns/MainActivity.kt
@@ -1,21 +1,12 @@
-
 package digital.overman.plcodingdesignpatterns
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import digital.overman.plcodingdesignpatterns.components.SingleRowSelector
-import digital.overman.plcodingdesignpatterns.components.Thousands
+import digital.overman.plcodingdesignpatterns.components.DataBoxItem
+import digital.overman.plcodingdesignpatterns.components.MarsWeather
+import digital.overman.plcodingdesignpatterns.components.MarsWeatherData
 import digital.overman.plcodingdesignpatterns.ui.theme.PLCodingDesignPatternsTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,22 +15,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PLCodingDesignPatternsTheme {
+                MarsWeather(
+                    MarsWeatherData(
+                        location = "Olympus Mons",
+                        description = "Dust Storm",
+                        descriptionIconResource = R.drawable.ic_action_name,
+                        currentTemp = -63,
+                        highTemp = -52,
+                        lowTemp = -73,
+                        dataBoxList = listOf(
+                            DataBoxItem(R.string.wind_speed_label, "27 km/h NW"),
+                            DataBoxItem(R.string.pressure_label, "600 Pa"),
+                            DataBoxItem(R.string.label_uv_radiation, "0.5 mSv/day"),
+                            DataBoxItem(R.string.martian_date_label, "914 Sol")
+                        )
+                    )
+                )
 //                MoodRateScreenDayNight()
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color(0xFFFEF7FF))
-                        .padding(16.dp),
-                    verticalArrangement = Arrangement.Center,
-                ) {
-                    Text(
-                        text = "Thousands separator",
-                    )
-                    SingleRowSelector(
-                        items = listOf(Thousands.PERIOD, Thousands.COMMA, Thousands.SPACE),
-                        onItemSelected = {}
-                    )
-                }
+//                Column(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .background(Color(0xFFFEF7FF))
+//                        .padding(16.dp),
+//                    verticalArrangement = Arrangement.Center,
+//                ) {
+//                    Text(
+//                        text = "Thousands separator",
+//                    )
+//                    SingleRowSelector(
+//                        items = listOf(Thousands.PERIOD, Thousands.COMMA, Thousands.SPACE),
+//                        onItemSelected = {}
+//                    )
+//                }
             }
         }
     }
